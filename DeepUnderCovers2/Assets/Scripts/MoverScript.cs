900ab02378b25c186224476a6baecbc3d4d542dc
Dan Kaiser <DanKaiser@Dans-MacBook.local>
2017-02-16 16:56:01 -0600

    Here is the arm movement script

---
+++ DeepUnderCovers2/Assets/Scripts/MoverScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax;
+}
+
+public class MoverScript : MonoBehaviour {
+
+	private Rigidbody2D rb;
+
+	public float speedOut;
+	public float speedIn;
+	public Boundary boundary;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 armMovement = new Vector3 (1.0f, 0.0f, 0.0f);
+		rb.position = new Vector3 
+			(
+				Mathf.Clamp (rb.position.x, boundary.xMin, boundary.xMax), 
+				0.0f, 
+				0.0f
+			);
+		if (Input.GetButton ("Fire1")) {
+
+			rb.velocity = armMovement * speedOut;
+
+
+		} else {
+
+			rb.velocity = -armMovement * speedIn;
+		}
+	}
+}
