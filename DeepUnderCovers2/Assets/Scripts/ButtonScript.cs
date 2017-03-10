7b06361c7c83e882151dfacf7874c37a7b96dff0
Dan Kaiser <DanKaiser@Dans-MacBook.local>
2017-03-10 16:39:33 -0600

    all pushed

---
+++ DeepUnderCovers2/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour {
+
+	bool pressed;
+	public float increaseHeatAmount = 0.2f;
+	public float decreaseHeatAmount = 0.2f;
+
+	void Start(){
+		pressed = false;
+	}
+
+	public void ButtonPressed() {
+		pressed = true;
+		MonsterMoverScript.MonsterButtonDown ();
+		MoverScript.MoverButtonDown ();
+	}
+
+	public void ButtonReleased(){
+		pressed = false;
+		MonsterMoverScript.MonsterButtonUp ();
+		MoverScript.MoverButtonUp ();
+	}
+
+	void Update(){
+
+		if (pressed) {
+			HeatControlScript.DecraseSlider (decreaseHeatAmount * Time.deltaTime);
+
+		} else {
+			HeatControlScript.IncreaseSlider (increaseHeatAmount * Time.deltaTime);
+		}
+
+	}
+
+	}
---
+++ DeepUnderCovers2/Assets/Scripts/LeftMonsterMoverScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftMonsterMoverScript : MonoBehaviour {
+
+
+	static private bool pressed;
+
+	public float speedRight;
+	public float speedLeft;
+	public MonsterBoundary boundary;
+
+	void Start(){
+		pressed = false;
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.CompareTag("Player"))
+			GameOver.LoseGame();
+	}
+
+	static public void MonsterButtonDown(){
+		pressed = true;
+	}
+
+	static public void MonsterButtonUp(){
+		pressed = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = new Vector3 
+			(
+				Mathf.Clamp (this.transform.position.x, boundary.leftBoundary, boundary.rightBoundary), 
+				this.transform.position.y, 
+				0.0f
+			);
+		if (pressed) {
+
+			this.transform.Translate(0.1f * speedRight, 0.0f, 0.0f);
+
+
+		} else {
+
+			this.transform.Translate(-0.1f * speedLeft, 0.0f, 0.0f);
+		}
+	}
+}
