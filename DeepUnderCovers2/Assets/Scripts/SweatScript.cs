a29fdbb0b0688ebbac8bb2549ad16ee79c849c8b
Dan Kaiser <DanKaiser@Dans-MacBook.local>
2017-03-17 16:59:10 -0500

    Added animations to indicate heat, removed the heat slider visuals, adjusted the background for the moon to be in the game scene instead of the background image on the slider.

---
+++ DeepUnderCovers2/Assets/Scripts/SweatScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SweatScript : MonoBehaviour {
+
+	public Slider heatSlider;
+
+	public Animator anim;
+
+
+	public float AnimatorSliderValue
+	{
+		get {
+			return anim.GetFloat (0);
+		}
+
+		set {
+			anim.SetFloat ("HeatSliderValue", value);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		AnimatorSliderValue = heatSlider.value;
+	}
+		
+}
