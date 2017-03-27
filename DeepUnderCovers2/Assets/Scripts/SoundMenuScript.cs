c2bd68f7ff3750cd6eb55789a443716e5bd42342
Dan Kaiser <DanKaiser@Dans-MacBook.local>
2017-03-26 22:42:08 -0500

    Made the start of a sound menu, cleaned up the buttons organization.

---
+++ DeepUnderCovers2/Assets/Scripts/SoundMenuScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundMenuScript : MonoBehaviour {
+
+	public GameObject soundMenu;
+	public GameObject xButton;
+
+	public void SoundMenuEnter() {
+
+		Time.timeScale = 0.0f;
+		soundMenu.gameObject.SetActive (true);
+		xButton.gameObject.SetActive (false);
+	}
+
+	public void SoundMenuExit() {
+
+		Time.timeScale = 1.0f;
+		soundMenu.gameObject.SetActive (false);
+		xButton.gameObject.SetActive (true);
+	}
+}
